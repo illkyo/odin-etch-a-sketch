76ea449b879769c8b4774704722bf436ceb567b7
illkyo <82893863+4thjonasbrother@users.noreply.github.com>
2025-01-04 20:58:03 +0500

    feat: add 16x16 grid with flexbox

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const h1 = document.querySelector(".header-red");
+// const h1 = document.querySelector(".header-red");
 
-h1.addEventListener('click', () => {
-  if (h1.classList.contains('header-red')) {
-    h1.classList.remove('header-red');
-    h1.classList.add('header-blue');
-  } else {
-    h1.classList.remove('header-blue');
-    h1.classList.add('header-red');
+// h1.addEventListener('click', () => {
+//   if (h1.classList.contains('header-red')) {
+//     h1.classList.remove('header-red');
+//     h1.classList.add('header-blue');
+//   } else {
+//     h1.classList.remove('header-blue');
+//     h1.classList.add('header-red');
+//   }
+// })
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+
+  for (let j = 0; j < 16; j++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    row.appendChild(square);
   }
-})
+
+  container.appendChild(row);
+} 
+
